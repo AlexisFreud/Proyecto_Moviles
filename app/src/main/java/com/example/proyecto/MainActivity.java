d3e1a7267894420b4030cd20c90274b308b38476
Angeljcc102 <angeljcc102@outlook.es>
2020-04-10 14:33:38 -0500

    Hola

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("HOLA MUNDO");
+        System.out.println("HOLA MUNDO");
+
 
     }
 }
