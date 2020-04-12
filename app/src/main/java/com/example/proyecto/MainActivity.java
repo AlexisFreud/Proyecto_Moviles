288e890bf2d570e9da6187aa4d4dbd8cf4d27eda
Angeljcc102 <angeljcc102@outlook.es>
2020-04-12 11:31:32 -0500

    tex2 removed

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -8,12 +8,9 @@ import android.view.View;
 import io.github.kexanie.library.MathView;
 
 public class MainActivity extends AppCompatActivity {
+    
     MathView formula_two;
     String tex = "This come from string. You can insert inline formula:" +
-            " \\(ax^2 + bx + c = 0\\) " +
-            "or displayed formula: $$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$";
-
-    String tex2 = " Minecraft tio This come from string. You can insert inline formula:" +
             " \\(ax^2 + bx + c = 0\\) " +
             "or displayed formula: $$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$";
 
