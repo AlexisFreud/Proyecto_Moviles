b6f9f72896d60cbd60495c59c0acd84ba61e1e9d
AlexisFreud <Alexis_valenciia@hotmail.com>
2020-05-15 12:02:56 -0500

    Fixed a problem with delete, and different inserts

--- app/src/main/java/com/example/proyecto/Ecuacion.java
+++ app/src/main/java/com/example/proyecto/Ecuacion.java
@@ -9,9 +9,26 @@ public class Ecuacion {
     public static final String[] signos = {"+", "-", "*", "/"};
     /*
     Estilo:
-        - x^n se escribe xn
-        - (ax+b)^n se escribe (ax+b)n
+        - Para escribir caracteres especiales se escriben entre %.
+          Ejemplo 1: x^n se traduce en
+                    polinomios = {x%e[n]%}
+          Ejemplo 2: ((ax+b)/(cx))/(dx+e) se traduce en
+                    polinomios = {%d[%d[ax+b][cx]%][dx+e]%}
      */
+        /*
+    Nota:
+        Lista de simbolos para funciones especiales:
+            - f:  fraccion
+            - e: exponencial (potencia)
+            - se: exponencial cuadrado (^2)
+            - sr: square root
+            - r: n root
+            - p: parentesis abre y cierra
+            - d: derivada
+            - ii: integral indefinida
+            - id: integral definida
+     */
+
 
     public Ecuacion() {
         polinomios = new LinkedList<>();
@@ -35,9 +52,11 @@ public class Ecuacion {
                 for (int i = 0; i < this.polinomios.size(); i++) {
                     posiciones += this.polinomios.get(i).length();
                     if (posiciones == this.position) {
+                        System.out.println("Insertar Between");
                         this.insertBetween(element, i);
                         break;
                     } else if (posiciones > this.position) {
+                        System.out.println("Insertar inside");
                         this.insertInside(element, i, this.position - (posiciones - this.polinomios.get(i).length()));
                         break;
                     }
@@ -50,6 +69,10 @@ public class Ecuacion {
         position++;
     }
 
+    public void insertSpecial(String identifier){
+
+    }
+
     public void delete()
     {
         if (position != 0 && numOfElements != 0)
@@ -58,22 +81,28 @@ public class Ecuacion {
             for (int i = 0; i < this.polinomios.size(); i++) {
                 posiciones += this.polinomios.get(i).length();
                 if (posiciones == this.position) { // Borrar ultimo elemento de String anterior
+                    System.out.println("Entroe a posiciones == position");
                     String polinomito = this.polinomios.get(i);
                     if (polinomito.length() == 1){
                         this.polinomios.remove(i);
+                        break;
                     }else{
                         this.polinomios.set(i, polinomito.substring(0, polinomito.length()-1));
+                        break;
                     }
                 } else if (posiciones > this.position) {
+                    System.out.println("Entro a posiciones > position");
                     String polinomito = this.polinomios.get(i);
                     if (polinomito.length() == 1){
                         this.polinomios.remove(i);
+                        break;
                     }else{
                         posiciones -= this.polinomios.get(i).length();
                         int posicionString = this.position-posiciones;
-                        String substringA = polinomito.substring(0, posicionString);
-                        String substringB = polinomito.substring(posicionString+1);
+                        String substringA = polinomito.substring(0, posicionString-1);
+                        String substringB = polinomito.substring(posicionString);
                         this.polinomios.set(i, substringA+substringB);
+                        break;
                     }
                 }
             }
@@ -114,7 +143,7 @@ public class Ecuacion {
             }
             else if(!isSigno(this.polinomios.get(listPosition)))
             {
-                this.polinomios.set(listPosition, element+this.polinomios.get(listPosition));
+                this.polinomios.set(listPosition, this.polinomios.get(listPosition)+element);
             }
             else
             {
@@ -128,9 +157,10 @@ public class Ecuacion {
     }
 
     private void insertEnd(String element) {
-        if (isSigno(this.polinomios.getLast())){ // Si el ultimo elem es signo, inserta
+        if (isSigno(this.polinomios.getLast()) || isSigno(element)){ // Si el ultimo elem es signo, inserta
             this.polinomios.add(element);
-        }else{
+        }
+        else{
             this.polinomios.set(this.polinomios.size()-1, this.polinomios.getLast()+element);
         }
     }
@@ -156,12 +186,6 @@ public class Ecuacion {
         return false;
     }
 
-    public void addEnd(String element){
-        if (!polinomios.isEmpty()){
-            polinomios.set(polinomios.size()-1, polinomios.getLast()+element);
-        }
-    }
-
     public void imprimeEcuacion()
     {
         if (polinomios.isEmpty())
@@ -172,7 +196,7 @@ public class Ecuacion {
         {
             for (int i = 0; i < polinomios.size(); i++)
             {
-                System.out.print(polinomios.get(i) + " ");
+                System.out.println(polinomios.get(i) + " ");
             }
             System.out.println();
         }
@@ -211,17 +235,8 @@ public class Ecuacion {
                 }
             }
         }
+        imprimeEcuacion();
         return "$$" + equation + "$$";
     }
 
-}class Polinomio{
-
-    public Polinomio(String poli){
-
-    }
-}class Fraccion{
-
-    public Fraccion(Polinomio numerador, Polinomio denominador){
-
-    }
 }
